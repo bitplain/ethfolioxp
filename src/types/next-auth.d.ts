5dae22270043cdd66c057006798615a204ac1d3a
kaiot <Psi12@me.com>
2026-01-16 16:10:11 +0300

    fix: extend next-auth types for role

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,11 +1,27 @@
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
+  interface User {
+    role: "USER" | "ADMIN";
+  }
+
   interface Session {
     user: {
       id: string;
       email: string;
       role: "USER" | "ADMIN";
     };
+  }
+}
+
+declare module "next-auth/adapters" {
+  interface AdapterUser {
+    role: "USER" | "ADMIN";
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: "USER" | "ADMIN";
   }
 }
